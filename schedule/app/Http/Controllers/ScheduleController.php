fe7cc266568cc18cbcab21d7522d831432d1a865
JakubKermes <jakub.kermes@studenci.collegiumwitelona.pl>
2023-05-07 18:21:42 +0200

    Update ScheduleController.php

--- schedule/app/Http/Controllers/ScheduleController.php
+++ schedule/app/Http/Controllers/ScheduleController.php
@@ -4,19 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Models\Lecturer;
 use App\Models\LectureSchedule;
+use App\Models\Legend;
 
 class ScheduleController extends Controller
 {
     public function getSchedule($id_group)
     {
+        $legends = new Legend();
         $schedule = LectureSchedule::where('id_group', $id_group)->get();
-        foreach ($schedule as $item) {
-         $lecturer = Lecturer::where('id_lecturer', $item->id_lecturer)->first()->name;
-         if($lecturer['name'] == 'Legenda'){
 
-         }
+        foreach ($schedule as $item){
+            $lecturer = Lecturer::where('id_lecturer', $item->id_lecturer)->first();
 
+            if ($lecturer->name == 'Legenda') {
+                $legends = Legend::where('id_group', $id_group)
+                    ->where('legend_name', $lecturer->name . $lecturer->lastname)
+                    ->get();
+            } else {
+                $legends = [];
+            }
         }
-        return response()->json($schedule, 200, [], JSON_UNESCAPED_UNICODE);
+
+        $response_data = [
+            'schedule' => $schedule,
+            'legends' => $legends
+        ];
+
+        return response()->json($response_data, 200, [], JSON_UNESCAPED_UNICODE);
     }
+
 }
