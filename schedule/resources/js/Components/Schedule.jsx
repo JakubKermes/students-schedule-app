4ff853cc327298bd69127df4d091b7355de18e45
Artur Flis <leeos10100@gmail.com>
2023-05-07 21:46:29 +0200

    Frontend update

--- schedule/resources/js/Components/Schedule.jsx
+++ schedule/resources/js/Components/Schedule.jsx
@@ -1,33 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import TextInput from "@/Components/TextInput";
 
 const localizer = momentLocalizer(moment);
 
-const Schedule = ({ lectures }) => {
+const Schedule = ({ lectures, onAddEvent }) => {
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [isAddingEvent, setIsAddingEvent] = useState(false);
+    const [newEvent, setNewEvent] = useState({ title: '', start: null, end: null });
+
+    const handleSelectDate = (date) => {
+        setSelectedDate(date);
+        setIsAddingEvent(true);
+        setNewEvent({ title: '', start: date, end: date });
+    };
+
+    const handleTitleChange = (event) => {
+        setNewEvent({ ...newEvent, title: event.target.value });
+    };
+
+    const handleStartChange = (date) => {
+        setNewEvent({ ...newEvent, start: date });
+    };
+
+    const handleEndChange = (date) => {
+        setNewEvent({ ...newEvent, end: date });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onAddEvent(newEvent);
+        setIsAddingEvent(false);
+    };
+
+    const handleClose = () => {
+        setIsAddingEvent(false);
+    };
+
     const events = Array.isArray(lectures)
-        ? lectures.map((lecture) => ({
-            id: lecture.id_lecture,
-            title: `Lecture #${lecture.id_lecture}`,
-            start: new Date(lecture.time_start),
-            end: new Date(lecture.time_end),
-        }))
+        ? lectures.reduce((acc, lecture) => {
+            if (lecture.hasOwnProperty('id_lecture')) {
+                acc.push({
+                    id: lecture.id_lecture,
+                    title: `${lecture.id_lecture}`,
+                    start: new Date(lecture.time_start),
+                    end: new Date(lecture.time_end),
+                });
+            }
+            return acc;
+        }, [])
         : [];
 
+    const eventPropGetter = (event) => {
+        return {
+            className: 'bg-blue-500 text-white rounded-md px-2 py-1 mb-1',
+        };
+    };
+
     return (
-        <div className="bg-white rounded-md shadow-md p-4">
+        <div className="bg-white rounded-md shadow-md p-4 text-black">
             <Calendar
                 localizer={localizer}
                 events={events}
                 startAccessor="start"
                 endAccessor="end"
                 className="h-96"
-                eventPropGetter={(event) => ({
-                    className: 'bg-blue-500 text-white rounded-md px-2 py-1 mb-1',
-                })}
+                eventPropGetter={eventPropGetter}
                 style={{ height: 500, width: '100%', margin: '0 auto' }}
+                selectable
+                onSelectSlot={(slotInfo) => handleSelectDate(slotInfo.start)}
             />
+
+            {isAddingEvent && (
+                <form onSubmit={handleSubmit}>
+                    <div className="mt-4">
+                        Title:<TextInput label="Title" value={newEvent.title} onChange={handleTitleChange} />
+                    </div>
+                    <div className="mt-4">
+                        <label htmlFor="start">Start:</label>
+                        <input type="datetime-local" id="start" value={moment(newEvent.start).format('YYYY-MM-DDTHH:mm')} onChange={(event) => handleStartChange(moment(event.target.value).toDate())} />
+                    </div>
+                    <div className="mt-4">
+                        <label htmlFor="end">End:</label>
+                        <input type="datetime-local" id="end" value={moment(newEvent.end).format('YYYY-MM-DDTHH:mm')} onChange={(event) => handleEndChange(moment(event.target.value).toDate())} />
+                    </div>
+                    <div className=".mt-4 flex justify-end">
+                        <button type="button" className="mr-2" onClick={handleClose}>
+                            Cancel
+                        </button>
+                        <button type="submit" className="bg-blue-500 text-white rounded-md px-2 py-1">
+                            Add Event
+                        </button>
+                    </div>
+                </form>
+            )}
         </div>
     );
 };
--- schedule/resources/js/Pages/Dashboard.jsx
+++ schedule/resources/js/Pages/Dashboard.jsx
@@ -3,20 +3,22 @@ import { Head } from '@inertiajs/react';
 import PrimaryButton from "@/Components/PrimaryButton";
 import Dropdown from "@/Components/Dropdown";
 import TextInput from "@/Components/TextInput";
+import Schedule from "@/Components/Schedule";
 
 export default function Dashboard(props) {
+    let onEventAdd;
     return (
         <AuthenticatedLayout
             auth={props.auth}
             errors={props.errors}
         >
-            <Head title="Dashboard" />
+            <Head title="Calendar" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
-
+                        <Schedule></Schedule>
                         </div>
                     </div>
                 </div>
