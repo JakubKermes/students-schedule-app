09ae6c74b2362fd8f20e8bc310a44d18cda10b71
Artur Flis <leeos10100@gmail.com>
2023-05-07 18:25:51 +0200

    Fixed: - Schedule.jsx (compatibility issues)

--- schedule/resources/js/Components/Schedule.jsx
+++ schedule/resources/js/Components/Schedule.jsx
@@ -6,21 +6,27 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 const localizer = momentLocalizer(moment);
 
 const Schedule = ({ lectures }) => {
-    const events = lectures.map((lecture) => ({
-        id: lecture.id_lecture,
-        title: `Lecture #${lecture.id_lecture}`,
-        start: new Date(lecture.time_start),
-        end: new Date(lecture.time_end),
-    }));
+    const events = Array.isArray(lectures)
+        ? lectures.map((lecture) => ({
+            id: lecture.id_lecture,
+            title: `Lecture #${lecture.id_lecture}`,
+            start: new Date(lecture.time_start),
+            end: new Date(lecture.time_end),
+        }))
+        : [];
 
     return (
-        <div>
+        <div className="bg-white rounded-md shadow-md p-4">
             <Calendar
                 localizer={localizer}
                 events={events}
                 startAccessor="start"
                 endAccessor="end"
-                style={{ height: 500 }}
+                className="h-96"
+                eventPropGetter={(event) => ({
+                    className: 'bg-blue-500 text-white rounded-md px-2 py-1 mb-1',
+                })}
+                style={{ height: 500, width: '100%', margin: '0 auto' }}
             />
         </div>
     );
