e5d98a503aa482a5ba5bdd9d24e029379a726ae2
JakubKermes <jakub.kermes@studenci.collegiumwitelona.pl>
2023-05-08 20:26:27 +0200

    Update UserEventController.php

--- schedule/app/Http/Controllers/UserEventController.php
+++ schedule/app/Http/Controllers/UserEventController.php
@@ -6,15 +6,17 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\UserEvent;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
 
 class UserEventController extends Controller
 {
     // Add user event from frontend
     public function store(Request $request)
     {
+        $id_laravel_user = Auth::id();
         // Validate the incoming request data
         $validator = Validator::make($request->all(), [
-            'id_laravel_user' => 'required|exists:users,id',
             'event_name' => 'required|string',
             'time_start' => 'required|date',
             'time_end' => 'required|date|after:time_start',
@@ -27,6 +29,7 @@ class UserEventController extends Controller
         // Create a new UserEvent instance
         $userEvent = new UserEvent();
         $userEvent->fill($request->all());
+        $userEvent->id_laravel_user = $id_laravel_user;
 
         // Save the new UserEvent to the database
         if ($userEvent->save()) {
@@ -34,5 +37,12 @@ class UserEventController extends Controller
         } else {
             return response()->json(['error' => 'Failed to add user event'], 500);
         }
+    }
+
+    public function getEvents()
+    {
+        $id_laravel_user = Auth::id();
+        $userEvents = UserEvent::where('id_laravel_user', $id_laravel_user)->get();
+        return response()->json($userEvents, 200, [], JSON_UNESCAPED_UNICODE);
     }
 }
