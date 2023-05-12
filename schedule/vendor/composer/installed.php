af942cf74360599a88a42589fa1ad95c47e3393a
JakubKermes <jakub.kermes@studenci.collegiumwitelona.pl>
2023-05-12 10:49:32 +0200

    console kernel fix

--- schedule/vendor/composer/installed.php
+++ schedule/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'c52bc1dbb03841c698bb4dfb8168bcccde426262',
+        'reference' => 'bbcc0b3a404742d72fbd090c1ab1069555beb499',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -445,7 +445,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'c52bc1dbb03841c698bb4dfb8168bcccde426262',
+            'reference' => 'bbcc0b3a404742d72fbd090c1ab1069555beb499',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
