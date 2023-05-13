3031a54cdaee512e7b6d559459a94d534847a2ff
Artur Flis <leeos10100@gmail.com>
2023-05-14 01:05:31 +0200

    Dashboard.jsx working too

--- schedule/resources/js/Pages/Dashboard.jsx
+++ schedule/resources/js/Pages/Dashboard.jsx
@@ -5,8 +5,24 @@ import Dropdown from "@/Components/Dropdown";
 import TextInput from "@/Components/TextInput";
 import Schedule from "@/Components/Schedule";
 import StudentGroupSelector from "@/Components/StudentGroupSelector";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 export default function Dashboard(props) {
+    const [lectures, setLectures] = useState(null);
+    const [groups, setGroups] = useState(null);
+
+    useEffect(() => {
+        axios.get(`/get_schedule/${groups}`).then((response) => {
+            setLectures(response.data.schedule);
+            console.log(groups)
+        });
+    } , [groups]);
+
+    const handleGroupsChange = (newGroups) => {
+        setGroups(newGroups);
+    }
+
     return (
         <AuthenticatedLayout
             auth={props.auth}
@@ -18,9 +34,9 @@ export default function Dashboard(props) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
-                            <StudentGroupSelector />
+                            <StudentGroupSelector onChange={handleGroupsChange} />
                             <br/>
-                            <Schedule />
+                            <Schedule lectures={lectures} />
                         </div>
                     </div>
                 </div>
